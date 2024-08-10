32a01750c14e1cb9ac2c894418be74f3b121dec1
Halil İbrahim Direktör <hidirektor@gmail.com>
2024-08-10 14:12:55 +0300

    Updated ESP Library

---
+++ v2.0-IoT/Core/Inc/ESP.h
@@ -0,0 +1,124 @@
+/*
+ * ESP.h
+ *
+ *  Created on: Aug 10, 2024
+ *      Author: hidirektor
+ */
+
+#ifndef INC_ESP_H_
+#define INC_ESP_H_
+
+#include "WifiConfig.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+#include "stm32f1xx_hal.h"
+#include "dwt_stm32_delay.h"
+
+typedef enum{false = 0, true = 1} bool;
+
+typedef	enum
+{
+	WifiMode_Error                        =     0,
+	WifiMode_Station                      =     1,
+	WifiMode_SoftAp                       =     2,
+	WifiMode_StationAndSoftAp             =     3,
+} WifiMode_t;
+
+typedef enum
+{
+  WifiEncryptionType_Open                 =     0,
+  WifiEncryptionType_WPA_PSK              =     2,
+  WifiEncryptionType_WPA2_PSK             =     3,
+  WifiEncryptionType_WPA_WPA2_PSK         =     4,
+} WifiEncryptionType_t;
+
+typedef enum
+{
+  WifiConnectionStatus_Error              =     0,
+  WifiConnectionStatus_GotIp              =     2,
+  WifiConnectionStatus_Connected          =     3,
+  WifiConnectionStatus_Disconnected       =     4,
+  WifiConnectionStatus_ConnectionFail     =     5,
+} WifiConnectionStatus_t;
+
+typedef struct
+{
+  WifiConnectionStatus_t      status;
+  uint8_t                     LinkId;
+  char                        Type[4];
+  char                        RemoteIp[17];
+  uint16_t                    RemotePort;
+  uint16_t                    LocalPort;
+  bool                        RunAsServer;
+} WifiConnection_t;
+
+typedef struct
+{
+	uint8_t                       usartBuff;
+	uint8_t                       RxBuffer[_WIFI_RX_SIZE];
+	uint8_t                       TxBuffer[_WIFI_TX_SIZE];
+	uint16_t                      RxIndex;
+	bool                          RxIsData;
+	WifiMode_t                    Mode;
+	char                          MyIP[16];
+	char                          MyGateWay[16];
+	char						  SSID_Connected[20];
+	bool                          StationDhcp;
+	char                          StationIp[16];
+	bool                          SoftApDhcp;
+	char                          SoftApConnectedDevicesIp[6][16];
+	char                          SoftApConnectedDevicesMac[6][18];
+	bool                          TcpIpMultiConnection;
+	uint16_t                      TcpIpPingAnswer;
+	WifiConnection_t              TcpIpConnections[5];
+} Wifi_t;
+
+extern Wifi_t	Wifi;
+
+void Wifi_RxClear(void);
+bool Wifi_SendString(char *data);
+bool Wifi_WaitForString(uint32_t TimeOut_ms,uint8_t *result,uint8_t CountOfParameter,...);
+void Wifi_RxCallBack(void);
+
+bool Wifi_Init(void);
+void Wifi_Enable(void);
+void Wifi_Disable(void);
+bool Wifi_Restart(void);
+bool Wifi_DeepSleep(uint16_t DelayMs);
+bool Wifi_FactoryReset(void);
+bool Wifi_Update(void);
+bool Wifi_SetRfPower(uint8_t Power_0_to_82);
+
+bool Wifi_SetMode(WifiMode_t	WifiMode_);
+bool Wifi_GetMode(void);
+bool Wifi_GetMyIp(void);
+
+bool Wifi_Station_ConnectToAp(char *SSID,char *Pass,char *MAC);
+bool Wifi_Station_Disconnect(void);
+bool Wifi_Station_DhcpEnable(bool Enable);
+bool Wifi_Station_DhcpIsEnable(void);
+bool Wifi_Station_SetIp(char *IP,char *GateWay,char *NetMask);
+
+bool Wifi_SoftAp_Create(char *SSID,char *password,uint8_t channel,
+		WifiEncryptionType_t WifiEncryptionType,uint8_t MaxConnections_1_to_4,
+		bool HiddenSSID);
+bool Wifi_GetApConnection(void);
+bool Wifi_SoftAp_GetConnectedDevices(void);
+
+bool Wifi_TcpIp_GetConnectionStatus(void);
+bool Wifi_TcpIp_Ping(char *PingTo);
+bool Wifi_TcpIp_SetMultiConnection(bool EnableMultiConnections);
+bool Wifi_TcpIp_GetMultiConnection(void);
+bool Wifi_TcpIp_StartTcpConnection(uint8_t LinkId,char *RemoteIp,uint16_t RemotePort,
+		uint16_t TimeOut_S);
+bool Wifi_TcpIp_StartUdpConnection(uint8_t LinkId,char *RemoteIp,uint16_t RemotePort,
+		uint16_t LocalPort);
+bool Wifi_TcpIp_Close(uint8_t LinkId);
+bool Wifi_TcpIp_SetEnableTcpServer(uint16_t PortNumber);
+bool Wifi_TcpIp_SetDisableTcpServer(uint16_t PortNumber);
+bool Wifi_TcpIp_SendDataUdp(uint8_t LinkId,uint16_t dataLen,uint8_t *data);
+bool Wifi_TcpIp_SendDataTcp(uint8_t LinkId,uint16_t dataLen,uint8_t *data);
+
+#endif /* INC_ESP_H_ */
--- v2.0-IoT/Core/Src/ESP8266.c
+++ v2.0-IoT/Core/Src/ESP8266.c
@@ -1,135 +1,102 @@
-#include "ESP8266.h"
+#include "ESP.h"
 
 #include "SystemDefaults.h"
 #include "GlobalVariables.h"
 #include "Flash.h"
 
 void ESP8266_Init(UART_HandleTypeDef *huart1, const char *wifiSS, const char *wifiPA) {
-	sprintf(bufferTX, "AT+RESTORE\r\n");
-	HAL_UART_Transmit_IT(huart1, (uint8_t*) bufferTX, strlen(bufferTX));
-	HAL_Delay(1000);
+    Wifi_FactoryReset();
+    HAL_Delay(1000);
 
-	sprintf(bufferTX, "AT+RST\r\n");
-	HAL_UART_Transmit_IT(huart1, (uint8_t*) bufferTX, strlen(bufferTX));
-	HAL_Delay(1000);
+    Wifi_Restart();
+    HAL_Delay(1000);
 
-	sprintf(bufferTX, "AT\r\n");
-	HAL_UART_Transmit_IT(huart1, (uint8_t*) bufferTX, strlen(bufferTX));
-	HAL_Delay(2000);
+    Wifi_SendString("AT\r\n");
+    HAL_Delay(2000);
 
-	sprintf(bufferTX, "AT+CWMODE=1\r\n");
-	HAL_UART_Transmit_IT(huart1, (uint8_t*) bufferTX, strlen(bufferTX));
-	HAL_Delay(2000);
+    Wifi_SetMode(WifiMode_Station);
+    HAL_Delay(2000);
 
-	char str[100];
-	strcpy(str, "AT+CWJAP_DEF=\"");
-	strcat(str, wifiSS);
-	strcat(str, "\",\"");
-	strcat(str, wifiPA);
-	strcat(str, "\"\r\n");
-	sprintf(bufferTX, "%s", str);
-	HAL_UART_Transmit_IT(huart1, (uint8_t*) bufferTX, strlen(bufferTX));
-	HAL_Delay(2000);
+    Wifi_Station_ConnectToAp((char *)wifiSS, (char *)wifiPA, NULL);
+    HAL_Delay(2000);
 }
 
 void ESP8266_Init_AP(UART_HandleTypeDef *huart1) {
-	sprintf(bufferTX, "AT+RESTORE\r\n");
-	HAL_UART_Transmit_IT(huart1, (uint8_t*) bufferTX, strlen(bufferTX));
-	HAL_Delay(1000);
+    Wifi_FactoryReset();
+    HAL_Delay(1000);
 
-	sprintf(bufferTX, "AT+RST\r\n");
-	HAL_UART_Transmit_IT(huart1, (uint8_t*) bufferTX, strlen(bufferTX));
-	HAL_Delay(1000);
+    Wifi_Restart();
+    HAL_Delay(1000);
 
-	sprintf(bufferTX, "AT\r\n");
-	HAL_UART_Transmit_IT(huart1, (uint8_t*) bufferTX, strlen(bufferTX));
-	HAL_Delay(2000);
-
-    // SoftAP + Client
-    sprintf(bufferTX, "AT+CWMODE=2\r\n");
-    HAL_UART_Transmit_IT(huart1, (uint8_t *)bufferTX, strlen(bufferTX));
+    Wifi_SendString("AT\r\n");
     HAL_Delay(2000);
 
-    //AP Name
-    sprintf(bufferTX, "AT+CWSAP=\"%s\",\"%s\",5,3\r\n", apSSID, apPassword);
-    HAL_UART_Transmit_IT(huart1, (uint8_t *)bufferTX, strlen(bufferTX));
+    Wifi_SetMode(WifiMode_SoftAp);
+    HAL_Delay(2000);
+
+    Wifi_SoftAp_Create(apSSID, apPassword, 5, WifiEncryptionType_WPA2_PSK, 4, false);
     HAL_Delay(2000);
 }
 
 void connectWifiNetwork(UART_HandleTypeDef *huart1, const char *wifiSS, const char *wifiPA) {
-	char str[100];
-	strcpy(str, "AT+CWJAP=\"");
-	strcat(str, wifiSS);
-	strcat(str, "\",\"");
-	strcat(str, wifiPA);
-	strcat(str, "\"\r\n");
-	sprintf(bufferTX, "%s", str);
-	HAL_UART_Transmit_IT(huart1, (uint8_t*) bufferTX, strlen(bufferTX));
-	HAL_Delay(2000);
+    Wifi_Station_ConnectToAp((char *)wifiSS, (char *)wifiPA, NULL);
+    HAL_Delay(2000);
 }
 
 void startWebServer(UART_HandleTypeDef *huart1) {
-    // Start the TCP on 2805
-    sprintf(bufferTX, "AT+CIPMUX=1\r\n");
-    HAL_UART_Transmit_IT(huart1, (uint8_t*) bufferTX, strlen(bufferTX));
-    HAL_Delay(2000);
-    sprintf(bufferTX, "AT+CIPSERVER=1,2805\r\n");
-    HAL_UART_Transmit_IT(huart1, (uint8_t*) bufferTX, strlen(bufferTX));
+    Wifi_TcpIp_SetMultiConnection(true);
     HAL_Delay(2000);
 
-    sprintf(bufferTX, "AT+CIPSEND=0,50");
-    HAL_UART_Transmit_IT(huart1, (uint8_t*) bufferTX, strlen(bufferTX));
+    Wifi_TcpIp_SetEnableTcpServer(2805);
     HAL_Delay(2000);
 
-    sprintf(bufferTX, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nHello from ESP8266 AP mode!");
-    HAL_UART_Transmit_IT(huart1, (uint8_t*) bufferTX, strlen(bufferTX));
+    uint8_t response[] = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nHello from ESP8266 AP mode!";
+    Wifi_TcpIp_SendDataTcp(0, sizeof(response), response);
     HAL_Delay(2000);
 
-    sprintf(bufferTX, "AT+CIPCLOSE=0");
-    HAL_UART_Transmit_IT(huart1, (uint8_t*) bufferTX, strlen(bufferTX));
+    Wifi_TcpIp_Close(0);
     HAL_Delay(2000);
 }
 
 void sendMachineData(UART_HandleTypeDef *huart1, const char *machineID, const char *wifiSSID, const char *wifiPass, const char *machineData) {
-	char local_txA[1500];
-	char local_txB[50];
-	int len;
-
-	char subMachineID[13];
-	strncpy(subMachineID, machineID, 12);
-	subMachineID[12] = '\0';
-
-	sprintf(bufferTX, "AT+CIPSTART=\"TCP\",\"%s\",3000\r\n", mainServer);
-	HAL_UART_Transmit_IT(huart1, (uint8_t*) bufferTX, strlen(bufferTX));
-	HAL_Delay(1000);
-
-	sprintf(local_txA,
-	    "GET /api/v2/machine/updateMachineRaw?machineID=%s&wifiSSID=%s&wifiPass=%s&devirmeYuruyusSecim=%d&calismaSekli=%d&emniyetCercevesi=%d&yavaslamaLimit=%d&altLimit=%d&kapiTablaAcKonum=%d&basincSalteri=%d&kapiSecimleri=%d&kapiAcTipi=%d&kapi1Tip=%d&kapi1AcSure=%d&kapi2Tip=%d&kapi2AcSure=%d&kapitablaTip=%d&kapiTablaAcSure=%d&yukariYavasLimit=%d&devirmeYukariIleriLimit=%d&devirmeAsagiGeriLimit=%d&devirmeSilindirTipi=%d&platformSilindirTipi=%d&yukariValfTmr=%d&asagiValfTmr=%d&devirmeYukariIleriTmr=%d&devirmeAsagiGeriTmr=%d&makineCalismaTmr=%d&buzzer=%d&demoMode=%d&calismaSayisi=%d&calismaSayisiDemo=%d&dilSecim=%d&eepromData38=%d&eepromData39=%d&eepromData40=%d&eepromData41=%d&eepromData42=%d&eepromData43=%d&eepromData44=%d&eepromData45=%d&eepromData46=%d&eepromData47=%d&lcdBacklightSure=%d HTTP/1.0\r\nHost: %s\r\n\r\n",
-	    subMachineID, wifiSSID, wifiPass, devirmeYuruyusSecim, calismaSekli, emniyetCercevesi,
-	    yavaslamaLimit, altLimit, kapiTablaAcKonum, basincSalteri, kapiSecimleri, kapiAcTipi, kapi1Tip, kapi1AcSure,
-	    kapi2Tip, kapi2AcSure, kapitablaTip, kapiTablaAcSure, yukariYavasLimit, devirmeYukariIleriLimit, devirmeAsagiGeriLimit,
-	    devirmeSilindirTipi, platformSilindirTipi, yukariValfTmr, asagiValfTmr, devirmeYukariIleriTmr, devirmeAsagiGeriTmr,
-	    makineCalismaTmr, buzzer, demoMode, calismaSayisi, calismaSayisiDemo, dilSecim, eepromData[38], eepromData[39],
-	    eepromData[40], eepromData[41], eepromData[42], eepromData[43], eepromData[44], eepromData[45],
-	    eepromData[46], eepromData[47], lcdBacklightSure, mainServerWithPort);
-
-	len = strlen(local_txA);
-	sprintf(local_txB, "AT+CIPSEND=%d\r\n", len);
-
-	HAL_UART_Transmit_IT(huart1, (uint8_t*) local_txB, strlen(local_txB));
-	HAL_Delay(2000);
-
-	HAL_UART_Transmit_IT(huart1, (uint8_t*) local_txA, strlen(local_txA));
-	HAL_Delay(2000);
-}
-
-void sendMachineDataDemo(UART_HandleTypeDef *huart1, const char *machineID, const char *wifiSSID, const char *wifiPass, const char *machineData) {
-    char bufferTX[4000];
+    char local_txA[1500];
     char local_txB[50];
-    char json_data[2000];
     int len;
 
-    // JSON verisini manuel olarak oluşturma
+    char subMachineID[13];
+    strncpy(subMachineID, machineID, 12);
+    subMachineID[12] = '\0';
+
+    Wifi_TcpIp_StartTcpConnection(0, mainServer, 3000, 10);
+    HAL_Delay(1000);
+
+    sprintf(local_txA,
+            "GET /api/v2/machine/updateMachineRaw?machineID=%s&wifiSSID=%s&wifiPass=%s&devirmeYuruyusSecim=%d&calismaSekli=%d&emniyetCercevesi=%d&yavaslamaLimit=%d&altLimit=%d&kapiTablaAcKonum=%d&basincSalteri=%d&kapiSecimleri=%d&kapiAcTipi=%d&kapi1Tip=%d&kapi1AcSure=%d&kapi2Tip=%d&kapi2AcSure=%d&kapitablaTip=%d&kapiTablaAcSure=%d&yukariYavasLimit=%d&devirmeYukariIleriLimit=%d&devirmeAsagiGeriLimit=%d&devirmeSilindirTipi=%d&platformSilindirTipi=%d&yukariValfTmr=%d&asagiValfTmr=%d&devirmeYukariIleriTmr=%d&devirmeAsagiGeriTmr=%d&makineCalismaTmr=%d&buzzer=%d&demoMode=%d&calismaSayisi=%d&calismaSayisiDemo=%d&dilSecim=%d&eepromData38=%d&eepromData39=%d&eepromData40=%d&eepromData41=%d&eepromData42=%d&eepromData43=%d&eepromData44=%d&eepromData45=%d&eepromData46=%d&eepromData47=%d&lcdBacklightSure=%d HTTP/1.0\r\nHost: %s\r\n\r\n",
+            subMachineID, wifiSSID, wifiPass, devirmeYuruyusSecim, calismaSekli, emniyetCercevesi,
+            yavaslamaLimit, altLimit, kapiTablaAcKonum, basincSalteri, kapiSecimleri, kapiAcTipi, kapi1Tip, kapi1AcSure,
+            kapi2Tip, kapi2AcSure, kapitablaTip, kapiTablaAcSure, yukariYavasLimit, devirmeYukariIleriLimit, devirmeAsagiGeriLimit,
+            devirmeSilindirTipi, platformSilindirTipi, yukariValfTmr, asagiValfTmr, devirmeYukariIleriTmr, devirmeAsagiGeriTmr,
+            makineCalismaTmr, buzzer, demoMode, calismaSayisi, calismaSayisiDemo, dilSecim, eepromData[38], eepromData[39],
+            eepromData[40], eepromData[41], eepromData[42], eepromData[43], eepromData[44], eepromData[45],
+            eepromData[46], eepromData[47], lcdBacklightSure, mainServerWithPort);
+
+    len = strlen(local_txA);
+    sprintf(local_txB, "AT+CIPSEND=%d\r\n", len);
+
+    Wifi_SendString(local_txB);
+    HAL_Delay(2000);
+
+    Wifi_SendString(local_txA);
+    HAL_Delay(2000);
+}
+
+
+void sendMachineDataDemo(UART_HandleTypeDef *huart1, const char *machineID, const char *wifiSSID, const char *wifiPass, const char *machineData) {
+    char json_data[2000];
+    char bufferTX[4000];
+    char local_txB[50];
+    int len;
+
     snprintf(json_data, sizeof(json_data),
              "{\"machineID\":\"%s\","
              "\"updateData\":{"
@@ -180,13 +147,11 @@ void sendMachineDataDemo(UART_HandleTypeDef *huart1, const char *machineID, cons
              machineID, wifiSSID, wifiPass, devirmeYuruyusSecim, calismaSekli, emniyetCercevesi, yavaslamaLimit, altLimit,
              kapiTablaAcKonum, basincSalteri, kapiSecimleri, kapiAcTipi, kapi1Tip, kapi1AcSure, kapi2Tip, kapi2AcSure,
              kapitablaTip, kapiTablaAcSure, yukariYavasLimit, devirmeYukariIleriLimit, devirmeAsagiGeriLimit,
-             devirmeSilindirTipi, platformSilindirTipi, yukariValfTmr, asagiValfTmr, devirmeYukariIleriTmr,
-             devirmeAsagiGeriTmr, makineCalismaTmr, buzzer, demoMode, calismaSayisi, calismaSayisiDemo, dilSecim,
-             eepromData[38], eepromData[39], eepromData[40], eepromData[41], eepromData[42], eepromData[43], eepromData[44], eepromData[45],
-             eepromData[46], eepromData[47], lcdBacklightSure);
-
-    sprintf(bufferTX, "AT+CIPSTART=\"TCP\",\"%s\",3000\r\n", mainServer);
-    HAL_UART_Transmit_IT(huart1, (uint8_t*) bufferTX, strlen(bufferTX));
+			 devirmeSilindirTipi, platformSilindirTipi, yukariValfTmr, asagiValfTmr, devirmeYukariIleriTmr,
+			 devirmeAsagiGeriTmr, makineCalismaTmr, buzzer, demoMode, calismaSayisi, calismaSayisiDemo, dilSecim,
+			 eepromData[38], eepromData[39], eepromData[40], eepromData[41], eepromData[42], eepromData[43], eepromData[44], eepromData[45],
+			 eepromData[46], eepromData[47], lcdBacklightSure);
+    Wifi_TcpIp_StartTcpConnection(0, mainServer, 3000, 10);
     HAL_Delay(4000);
 
     snprintf(bufferTX, sizeof(bufferTX),
@@ -196,127 +161,110 @@ void sendMachineDataDemo(UART_HandleTypeDef *huart1, const char *machineID, cons
              "Content-Length: %d\r\n\r\n"
              "%s",
              mainServerWithPort, strlen(json_data), json_data);
+
     len = strlen(bufferTX);
     sprintf(local_txB, "AT+CIPSEND=%d\r\n", len);
 
-    HAL_UART_Transmit_IT(huart1, (uint8_t*) local_txB, strlen(local_txB));
+    Wifi_SendString(local_txB);
     HAL_Delay(4000);
 
-    HAL_UART_Transmit_IT(huart1, (uint8_t*) bufferTX, strlen(bufferTX));
+    Wifi_SendString(bufferTX);
     HAL_Delay(15000);
 }
 
 int checkMachineID(UART_HandleTypeDef *huart1, const char *machineID) {
-    char local_txA[1000];
-    char local_txB[50];
-    int len;
+	char local_txA[1000];
+	char local_txB[50];
+	int len;
+	char bufferRX[1500];
 
-    char bufferRX[1500];
+	char subMachineID[13];
+	strncpy(subMachineID, machineID, 12);
+	subMachineID[12] = '\0';
 
-    char subMachineID[13];
-    strncpy(subMachineID, machineID, 12);
-    subMachineID[12] = '\0';
+	Wifi_TcpIp_StartTcpConnection(0, mainServer, 3000, 10);
+	HAL_Delay(1000);
 
-    sprintf(bufferTX, "AT+CIPSTART=\"TCP\",\"%s\",3000\r\n", mainServer);
-    HAL_UART_Transmit_IT(huart1, (uint8_t*) bufferTX, strlen(bufferTX));
-    HAL_Delay(1000);
+	sprintf(local_txA,
+			"GET /api/v2/machine/checkMachineIDRaw?machineID=%s HTTP/1.0\r\nHost: %s\r\n\r\n", subMachineID, mainServerWithPort);
 
-    sprintf(local_txA,
-            "GET /api/v2/machine/checkMachineIDRaw?machineID=%s HTTP/1.0\r\nHost: %s\r\n\r\n", subMachineID, mainServerWithPort);
+	len = strlen(local_txA);
+	sprintf(local_txB, "AT+CIPSEND=%d\r\n", len);
 
-    len = strlen(local_txA);
-    sprintf(local_txB, "AT+CIPSEND=%d\r\n", len);
+	Wifi_SendString(local_txB);
+	HAL_Delay(2000);
 
-    HAL_UART_Transmit_IT(huart1, (uint8_t*) local_txB, strlen(local_txB));
-    HAL_Delay(2000);
+	Wifi_SendString(local_txA);
+	HAL_Delay(2000);
 
-    HAL_UART_Transmit_IT(huart1, (uint8_t*) local_txA, strlen(local_txA));
-    HAL_Delay(2000);
+	HAL_UART_Receive_IT(huart1, (uint8_t *)bufferRX, sizeof(bufferRX));
+	HAL_Delay(5000);
 
-    HAL_UART_Receive_IT(huart1, bufferRX, sizeof(bufferRX));
-    HAL_Delay(5000);
-
-    // HTTP yanıt kodunu kontrol et
-    if (strstr(bufferRX, "HTTP/1.1 200 OK")) {
-        return 1;  // Başarılı
-    } else {
-        return 0;  // Başarısız
-    }
+	if (strstr(bufferRX, "HTTP/1.1 200 OK")) {
+		return 1;  // Başarılı
+	} else {
+		return 0;  // Başarısız
+	}
 }
 
 void checkForUpdates(UART_HandleTypeDef *huart, const char *currentVersion) {
-    char checkUpdateCommand[150];
-    sprintf(checkUpdateCommand, "GET /api/v2/updateChecker/checkUpdatesRaw?currentVersion=%s HTTP/1.0\r\nHost: %s\r\n\r\n", currentVersion, mainServerWithPort);
+	char checkUpdateCommand[150];
+	sprintf(checkUpdateCommand, "GET /api/v2/updateChecker/checkUpdatesRaw?currentVersion=%s HTTP/1.0\r\nHost: %s\r\n\r\n", currentVersion, mainServerWithPort);
+	Wifi_TcpIp_StartTcpConnection(0, mainServer, 3000, 10);
+	HAL_Delay(2000);
 
-    // ESP8266 AT komutları ile sunucuya bağlan
-    sprintf(bufferTX, "AT+CIPSTART=\"TCP\",\"%s\",3000\r\n", mainServer);
-    HAL_UART_Transmit_IT(huart, (uint8_t*)bufferTX, strlen(bufferTX));
-    HAL_Delay(2000);
+	sprintf(bufferTX, "AT+CIPSEND=%d\r\n", strlen(checkUpdateCommand));
+	Wifi_SendString(bufferTX);
+	HAL_Delay(2000);
 
-    // Güncelleme kontrol isteğini gönder
-    sprintf(bufferTX, "AT+CIPSEND=%d\r\n", strlen(checkUpdateCommand));
-    HAL_UART_Transmit_IT(huart, (uint8_t*)bufferTX, strlen(bufferTX));
-    HAL_Delay(2000);
-    HAL_UART_Transmit_IT(huart, (uint8_t*)checkUpdateCommand, strlen(checkUpdateCommand));
-    HAL_Delay(5000);
+	Wifi_SendString(checkUpdateCommand);
+	HAL_Delay(5000);
 
-    // Cevabı al ve işle
-    HAL_UART_Receive_IT(huart, bufferRX, sizeof(bufferRX));
-    HAL_Delay(5000);
+	HAL_UART_Receive_IT(huart, bufferRX, sizeof(bufferRX));
+	HAL_Delay(5000);
 
-    // Gelen veriyi işleyin ve güncelleme var mı kontrol edin
-    if (strstr(bufferRX, "HTTP/1.1 200 OK")) {
-        // Yanıt başarılı, güncelleme bilgilerini kontrol et
-        if (strstr(bufferRX, "\"updateAvailable\":true")) {
-            lcd_print(1, 1, "New Update Available");
-            HAL_Delay(2000);
-            lcd_clear();
-            //downloadNewVersion(huart, "2.0.0");  // Versiyon numarasını dinamik olarak belirleyebilirsiniz
-        } else {
-            lcd_print(1, 1, "No Update Available");
-            HAL_Delay(2000);
-            lcd_clear();
-        }
-    } else {
-        // Yanıt başarısız, hata mesajı göster
-        lcd_print(1, 1, "Update Check Failed");
-        HAL_Delay(2000);
-        lcd_clear();
-    }
+	if (strstr(bufferRX, "HTTP/1.1 200 OK")) {
+		if (strstr(bufferRX, "\"updateAvailable\":true")) {
+			lcd_print(1, 1, "New Update Available");
+			HAL_Delay(2000);
+			lcd_clear();
+		} else {
+			lcd_print(1, 1, "No Update Available");
+			HAL_Delay(2000);
+			lcd_clear();
+		}
+	} else {
+		lcd_print(1, 1, "Update Check Failed");
+		HAL_Delay(2000);
+		lcd_clear();
+	}
 }
 
 void downloadNewVersion(UART_HandleTypeDef *huart, const char *versionCode) {
-    char downloadCommand[150];
-    sprintf(downloadCommand, "GET /api/v2/updateChecker/downloadNewVersionRaw?versionCode=%s HTTP/1.0\r\nHost: 85.95.231.92:3000\r\n\r\n", versionCode);
+	char downloadCommand[150];
+	sprintf(downloadCommand, "GET /api/v2/updateChecker/downloadNewVersionRaw?versionCode=%s HTTP/1.0\r\nHost: 85.95.231.92:3000\r\n\r\n", versionCode);
+	Wifi_TcpIp_StartTcpConnection(0, "85.95.231.92", 3000, 10);
+	HAL_Delay(2000);
 
-    // Sunucuya bağlan
-    sprintf(bufferTX, "AT+CIPSTART=\"TCP\",\"85.95.231.92\",3000\r\n");
-    HAL_UART_Transmit_IT(huart, (uint8_t*)bufferTX, strlen(bufferTX));
-    HAL_Delay(2000);
+	sprintf(bufferTX, "AT+CIPSEND=%d\r\n", strlen(downloadCommand));
+	Wifi_SendString(bufferTX);
+	HAL_Delay(2000);
 
-    // İndirme isteğini gönder
-    sprintf(bufferTX, "AT+CIPSEND=%d\r\n", strlen(downloadCommand));
-    HAL_UART_Transmit_IT(huart, (uint8_t*)bufferTX, strlen(bufferTX));
-    HAL_Delay(2000);
-    HAL_UART_Transmit_IT(huart, (uint8_t*)downloadCommand, strlen(downloadCommand));
-    HAL_Delay(5000);
+	Wifi_SendString(downloadCommand);
+	HAL_Delay(5000);
 
-    // Cevabı al ve .hex dosyasını indir
-    HAL_UART_Receive_IT(huart, bufferRX, sizeof(bufferRX));
-    HAL_Delay(5000);
+	HAL_UART_Receive_IT(huart, bufferRX, sizeof(bufferRX));
+	HAL_Delay(5000);
 
-    // Burada .hex dosyasını alıp veriyi bir buffer'a saklamalısınız
-    uint32_t receivedData[1024]; // Bu buffer'ı gelen verinin büyüklüğüne göre ayarlamalısınız.
-    int dataSize = 0; // Gelen verinin büyüklüğü burada tutulacak
+	uint32_t receivedData[1024];
+	int dataSize = 0;
 
-    // bufferRX verisini parse et ve receivedData'ya aktar
-    parseHexDataToBuffer(bufferRX, receivedData, &dataSize);
+	parseHexDataToBuffer(bufferRX, receivedData, &dataSize);
 
-    // İndirilen veriyi flash belleğe yazma işlemi
-    lcd_print(1, 1, "Writing to Flash...");
-    HAL_Delay(2000);
+	lcd_print(1, 1, "Writing to Flash...");
+	HAL_Delay(2000);
 
-    writeFlash(receivedData, dataSize);
+	writeFlash(receivedData, dataSize);
 }
 
 
